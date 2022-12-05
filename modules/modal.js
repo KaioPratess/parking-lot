7e12cca979d775359f900409ff416d8cfe684622
Kaio Prates <kaio_prates@hotmail.com>
2022-12-05 17:39:18 -0300

    Calcular horário e valor

--- modules/modal.js
+++ modules/modal.js
@@ -3,6 +3,9 @@ export default (function showModal() {
   const manageBtn = document.querySelector('.manage-btn');
   const closeModal = document.querySelector('.close-modal');
   const tbody = document.querySelector('.tbody');
+  let startDate;
+  let endDate;
+  let hours;
 
   async function getData() {
     const data = await fetch(
@@ -17,10 +20,24 @@ export default (function showModal() {
     response.forEach((rental) => {
       const tr = document.createElement('tr');
       const keys = Object.keys(rental);
+
       keys.forEach((key) => {
         const td = document.createElement('td');
         if (typeof rental[key] !== 'object') {
           td.textContent = rental[key];
+          if (key == 'startDate') {
+            startDate = new Date(rental[key]);
+          } else if (key == 'endDate' && rental[key] !== null) {
+            endDate = new Date(rental[key]);
+          } else if (key == 'hour' && rental.endDate !== null) {
+            const startHour = startDate.getUTCHours();
+            const endHour = endDate.getUTCHours();
+            hours = endHour - startHour;
+            td.textContent = `${hours}h`;
+          } else if (key == 'value' && rental.endDate !== null) {
+            const hourValue = 10;
+            td.textContent = `R$ ${hours * hourValue}`;
+          }
         } else {
           if (rental[key]) {
             td.textContent = rental[key].plateNumber;
